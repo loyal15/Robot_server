87020042f1966c628b5b9229e34bf4a04f353861
suisun <suisun_312@yahoo.co.jp>
2018-11-05 14:41:48 +0800

    fix more

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 
 // 'use strict';
-// import express from 'express';
-// import path from 'path';
-// import bodyParser from 'body-parser';
-// import { listeners } from 'cluster';
 
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-    res.send("<h1>Hello world'</h1>");
+    // res.send("<h1>Hello world</h1>");
+    res.sendfile('/.html/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
 });
 
 http.listen(4500, function() {
