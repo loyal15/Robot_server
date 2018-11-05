72e802359ace4993b72bbf74db1d2836bc1e563b
suisun <suisun_312@yahoo.co.jp>
2018-11-05 10:41:23 +0800

    test

--- server.js
+++ server.js
@@ -1,35 +1,40 @@
 
 // 'use strict';
 import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { listeners } from 'cluster';
 
-//-------------------------------------------------------//
-var Hapi = require('hapi');
-//-------------------------------------------------------//
-var port = 80;
-var server = new Hapi.Server({ port: port });
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const PORT = 4500;
 
-var portLive = 4500;
-var serverLive = new Hapi.Server({ port:portLive});
+server.listen(PORT);
+console.log('Server is running');
 
-const init = async() => {
-    await server.register(require('inert'));
-    await server.start();    
-    console.log('Robot Backend(RESTful API) Server has started on port ' + port);
-}
-const initLive = async() => {
-    await serverLive.start();    
-    console.log('RobotApp Server has started on port ' + portLive);
-    require('./robot-server/socket_io_server')(serverLive);
+app.get('/', (req, res)) => {
+    res.file('/.html/index.html');
 }
 
-server.route({method:'GET',path:'/',handler: (req,res) => { 
-    return res.file('./html/index.html');
-}});
+
+// const init = async() => {
+//     await server.register(require('inert'));
+//     await server.start();    
+//     console.log('Robot Backend(RESTful API) Server has started on port ' + port);
+// }
+// const initLive = async() => {
+//     await serverLive.start();    
+//     console.log('RobotApp Server has started on port ' + portLive);
+//     require('./robot-server/socket_io_server')(serverLive);
+// }
+
+// server.route({method:'GET',path:'/',handler: (req,res) => { 
+//     return res.file('./html/index.html');
+// }});
 
 
-init();
-initLive();
+// init();
+// initLive();
 
-var d = new Date();
-console.log( d.getTime());
 
