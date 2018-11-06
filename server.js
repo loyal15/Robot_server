6fd775adcab4084ad0df34c44f7332f0b0da4aa3
suisun <suisun_312@yahoo.co.jp>
2018-11-06 10:33:09 +0800

    more

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 4500;
 
 app.get('/', function(req, res) {
     res.sendfile('./html/index.html');
@@ -17,54 +18,11 @@ io.on('connection', function(socket) {
 
         // broadcast
         // io.emit('message', msg);
-        io.send('message456', msg);
+        io.send(msg);
     });
 });
 
-http.listen(4500, function() {
-   console.log('listening on *:4500');
+http.listen(port, function() {
+   console.log('listening on :' + port);
 });
-
-
-
-
-
-
-
-
-
-// var server = require('http').createServer(app);
-// var io = require('socket.io').listen(server);
-// var PORT = 4500;
-
-// server.listen(PORT);
-// console.log('Server is running');
-
-// server.route({method:'GET',path:'/',handler: (req,res) => { 
-//     return res.file('./html/index.html');
-// }});
-
-// app.get('/', (req, res)) => {
-//     res.file('/.html/index.html');
-// }
-
-// const init = async() => {
-//     await server.register(require('inert'));
-//     await server.start();    
-//     console.log('Robot Backend(RESTful API) Server has started on port ' + port);
-// }
-// const initLive = async() => {
-//     await serverLive.start();    
-//     console.log('RobotApp Server has started on port ' + portLive);
-//     require('./robot-server/socket_io_server')(serverLive);
-// }
-
-// server.route({method:'GET',path:'/',handler: (req,res) => { 
-//     return res.file('./html/index.html');
-// }});
-
-
-// init();
-// initLive();
-
 
