96b22fe4af06a5b9e23965b0ac040491966a21ab
suisun <suisun_312@yahoo.co.jp>
2018-11-06 11:08:38 +0800

    add more

--- server.js
+++ server.js
@@ -5,20 +5,57 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 4500;
+var clients = [];
 
 app.get('/', function(req, res) {
     res.sendfile('./html/index.html');
 });
 
-io.on('connection', function(socket) {
-    console.log('A user connected');
+// io.on('connection', function(socket) {
+//     console.log('A user connected');
+
+//     socket.on('message', function(msg) {
+//         console.log('message:' + msg);
+
+//         // broadcast
+//         // io.emit('message', msg);
+//         io.send(msg);
+//     });
+// });
+
+io.sockets.on('connection', function(socket) {
+    socket.on('login', function(data) {
+        var clientInfo;
+        clientInfo.uid = data.uid;
+        clientInfo.id = socket.id;
+
+        clients.push(clientInfo);
+
+        console.log(clientInfo.uid + ' connected');
+    });
 
     socket.on('message', function(msg) {
-        console.log('message:' + msg);
 
-        // broadcast
-        // io.emit('message', msg);
-        io.send(msg);
+        for (var i = 0; i < clients.length; i++) {
+            var client = clients[i];
+            console.log('client.uid = ' + client.uid);
+
+            if (client.uid == data.uid) {
+                io.sockets.socket(client.id).send(data.msg);
+                break;
+            }
+        }
+    });
+
+    socket.on('disconnect', function() {
+        for (var i = 0; i < clients.length; i++) {
+            var client = clients[i];
+            if (client.id == socket.id) {
+                clients.splice(i, 1);
+                break;
+            }
+        }
+        console.log('User disconnected');
     });
 });
 
